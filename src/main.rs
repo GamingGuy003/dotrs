b3fbdd9b563be86f59c69712daec5acd9bb8005d
GamingGuy003 <bjornfelixh@gmail.com>
2024-02-10 13:52:02 +0000

    Added initial cli arguments for testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use cali::parser::Parser;
+use log::error;
+use std::process::exit;
+
+mod git;
+mod profiles;
+
 fn main() {
-    println!("Hello, world!");
+    pretty_env_logger::init_timed();
+    let mut parser = Parser::new()
+        .add_arg("h", "help", "Prints this help prompt", false, None)
+        .add_arg("i", "init", "Initializes repo at location", true, None);
+
+    parser.parse().unwrap_or_else(|err| {
+        error!("{}", err);
+        exit(-1);
+    });
+
+    match parser.get_parsed_argument_long("help") {
+        (true, _) => parser
+            .get_arguments()
+            .iter()
+            .for_each(|argument| println!("\t{argument}")),
+        _ => {}
+    }
+
+    match parser.get_parsed_argument_long("init") {
+        (true, Some(path)) => {
+            println!("{path}")
+        },
+        _ => {}
+    }
 }
